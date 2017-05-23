70b9c6d810e886a73ce4ce59e958902e99ec63b2
amansinghal360 <aman.singhal@tothenew.com>
2017-05-23 12:29:10 +0530

    second commit

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,20 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SampleTest {
+
+	@Test
+	public void firstTest() {
+		int a = 1;
+		int b = 2;
+		Assert.assertTrue(a + b == 3);
+	}
+
+	@Test
+	public void SecondTest() {
+		int a = 1;
+		int b = 2;
+		Assert.assertTrue(a + b == 3);
+	}
+}
+ 
---
+++ src/test/java/SecondTest.java
@@ -0,0 +1,36 @@
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class SecondTest {
+	
+	private WebDriver driver;
+	
+	@Before	
+	public void setUp()
+	{
+		System.setProperty("webdriver.chrome.driver", "E://chromedriver.exe");
+		driver=new ChromeDriver();
+	}
+	
+	
+	@After
+	public void tearDown()
+	{ 
+		driver.quit();
+	}
+	
+	@Test
+	public void shouldLoadFirstPage()
+	{
+		driver.navigate().to("https://www.youtube.com/watch?v=CRvwKHUVJTQ&list=PL_noPv5wmuO-oOO2-czJsqVbPAKnmT_7h&index=2");
+		String getTitle = driver.getTitle();
+		Assert.assertTrue(getTitle.contains("Jenkins Tutorial "));
+	}
+
+}
